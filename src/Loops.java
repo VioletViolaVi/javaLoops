90e54901682b84f30a049bf7b893df49058e3856
VioletViola <vivian.v18723@gmail.com>
2021-08-16 18:56:12 +0100

    Practice both while and for loop types.

--- src/Loops.java
+++ src/Loops.java
@@ -1,5 +1,38 @@
 public class Loops {
     public static void main(String[] args) {
+        // while loop
+        int counter = 1;
+        System.out.println("This is a While Loop");
+        while (counter < 11) {
+            if (counter % 2 == 0) {
+                System.out.println(counter + " is an Even Number!");
+            } else {
+                System.out.println(counter + " is an Odd Number!");
+            }
+            counter++;
+        }
 
+        System.out.println("\n");
+
+        // do while loop
+        int count = 1;
+        do {
+            System.out.println("This do while loop will only run once!");
+            count++;
+        } while (count == 0);
+
+        System.out.println("\n");
+
+        // for loops
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\n");
+
+        String[] heroes = {"raven", "beastboy", "robin", "cyborg", "starfire", "storm", "hercules", "arrow", "killer frost", "catwomen"};
+        for (int i = 0; i < heroes.length; i++) {
+            System.out.println(i+1 + ". " + heroes[i]);
+        }
     }
 }
