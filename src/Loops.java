a388e7dc11047fcfe366a95bf60a0e270c932bba
VioletViola <vivian.v18723@gmail.com>
2021-08-16 20:48:57 +0100

    Add for each loop for ages.

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Loops {
     public static void main(String[] args) {
         // while loop
@@ -44,6 +46,39 @@ public class Loops {
         for (String hero : heroes) {
             num++;
             System.out.println(num + ". " + hero);
+        }
+
+        System.out.println("\n");
+
+        // for each loop
+        ArrayList<String> colours = new ArrayList<>();
+        colours.add("red");
+        colours.add("green");
+        colours.add("brown");
+        colours.add("blue");
+        colours.add("purple");
+        colours.add("yellow");
+        colours.add("pink");
+        colours.add("white");
+        colours.add("orange");
+        colours.add("black");
+        colours.add("gold");
+        for (String colour : colours) {
+            System.out.println(colour);
+        }
+
+        System.out.println("\n");
+
+        // for each loop
+        ArrayList<Integer> ages = new ArrayList<>();
+        ages.add(56);
+        ages.add(26);
+        ages.add(34);
+        ages.add(99);
+        ages.add(41);
+        ages.add(87);
+        for (int age : ages) {
+            System.out.println(age);
         }
 
     }
